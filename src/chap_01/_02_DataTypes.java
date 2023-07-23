e99b723a3d59b0c2091b352fba5ff3acf1630490
note9999 <sanghoon703@naver.com>
2023-07-23 15:35:21 +0900

    javaFoundation

---
+++ src/chap_01/_02_DataTypes.java
@@ -0,0 +1,15 @@
+package chap_01;
+
+public class _02_DataTypes {
+    public static void main(String[] args) {
+        System.out.println("Test2");   //문자열
+        System.out.println("Test3");
+        System.out.println(12);        //숫자
+        System.out.println(-34);
+        System.out.println(3.14);
+        System.out.println(true);       //Boolean
+        System.out.println(false);
+
+        System.out.println(123+345);
+    }
+}
